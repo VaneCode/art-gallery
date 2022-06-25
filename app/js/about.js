9d44e1112bfebe865a79d94816cf0aaea3920009
vanessaBonito <vanebonito@hotmail.com>
2022-06-25 18:56:04 -0500

    Add css styles in certification section in about.html file

---
+++ app/js/about.js
@@ -0,0 +1,38 @@
+//Collections
+const collection1 =[
+    {
+        pictureID:1,
+        img:'./images/c1-img1.jpg'
+    },
+    {
+        pictureID:2,
+        img:'./images/c1-img2.jpg'
+    },
+    {
+        pictureID:3,
+        img:'./images/c1-img3.jpg'
+    },
+    {
+        pictureID:1,
+        img:'./images/c1-img4.jpg'
+    }
+];
+
+const collection2 =[
+    {
+        sculptureID:1,
+        img:'./images/c2-img1.jpg'
+    },
+    {
+        sculptureID:2,
+        img:'./images/c2-img2.jpg'
+    },
+    {
+        sculptureID:3,
+        img:'./images/c2-img3.jpg'
+    },
+    {
+        sculptureID:4,
+        img:'./images/c2-img4.jpg'
+    }
+];
