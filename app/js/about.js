9d2dbac16380d5706d67a0709b650c2fbac4d3bd
vanessaBonito <vanebonito@hotmail.com>
2022-06-27 17:48:58 -0500

    Fix linter errors

--- app/js/about.js
+++ app/js/about.js
@@ -1,4 +1,4 @@
-//Variables and constants declaration
+// Variables and constants declaration
 const carrContainer = document.querySelector('.carr-container');
 const points = document.querySelectorAll('.point');
 const carrImg = document.querySelector('.carr-img');
@@ -6,44 +6,43 @@ const arrows = document.querySelectorAll('.arrow');
 let position = 0;
 let position1 = 0;
 
-//Function to move the div horizontally
+// Function to move the div horizontally
 function moveCarr(element, pos) {
-    let calcMove = 0;
-    //Calculate the space that carrContainer will move
-    calcMove = pos * -25;
-    //Move carrContainer to show the next image
-    element.style.transform = `translateX(${calcMove}%)`;
-
+  let calcMove = 0;
+  // Calculate the space that carrContainer will move
+  calcMove = pos * -25;
+  // Move carrContainer to show the next image
+  element.style.transform = `translateX(${calcMove}%)`;
 }
 
-//Function to change arrows icons styles       
+// Function to change arrows icons styles
 function activeArrows(points, i) {
-    points.forEach((point) => {
-        point.classList.remove('active');
-    });
-    points[i].classList.add('active');
+  points.forEach((point) => {
+    point.classList.remove('active');
+  });
+  points[i].classList.add('active');
 }
 
 points.forEach((point, i) => {
-    point.addEventListener('click', () => {
-        //Restart the position 
-        if (position > 3) {
-            position = 0;
-        }
-        moveCarr(carrContainer, position);
-        activeArrows(points, i);
-        position++;
-    });
+  point.addEventListener('click', () => {
+    // Restart the position
+    if (position > 3) {
+      position = 0;
+    }
+    moveCarr(carrContainer, position);
+    activeArrows(points, i);
+    position += 1;
+  });
 });
 
 arrows.forEach((arrow, i) => {
-    arrow.addEventListener('click', () => {
-        //Restart the position 
-        if (position1 > 3) {
-            position1 = 0;
-        }
-        moveCarr(carrImg, position1);
-        activeArrows(arrows, i);
-        position1++;
-    });
+  arrow.addEventListener('click', () => {
+    // Restart the position
+    if (position1 > 3) {
+      position1 = 0;
+    }
+    moveCarr(carrImg, position1);
+    activeArrows(arrows, i);
+    position1 += 1;
+  });
 });
--- app/js/menu.js
+++ app/js/menu.js
@@ -3,17 +3,17 @@ const burgerIcon = document.querySelector('.nav-icon-toggle');
 const menuOptions = document.querySelectorAll('.menu-option');
 
 function showMenu() {
-    mobileMenu.classList.toggle('display-menu');
-    if (mobileMenu.classList.contains('display-menu')) {
-        burgerIcon.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-    } else {
-        burgerIcon.innerHTML = '<i class="fa-solid fa-bars"></i>';
-    }
+  mobileMenu.classList.toggle('display-menu');
+  if (mobileMenu.classList.contains('display-menu')) {
+    burgerIcon.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  } else {
+    burgerIcon.innerHTML = '<i class="fa-solid fa-bars"></i>';
+  }
 }
 
 burgerIcon.addEventListener('click', showMenu);
 menuOptions.forEach(
-    (option) => {
-      option.addEventListener('click', showMenu);
-    }
-  );
+  (option) => {
+    option.addEventListener('click', showMenu);
+  },
+);
